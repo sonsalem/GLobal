1f02303e520f88d1ca048c81ed882c379d8f933f
SonSalem <salemmamdouh77777@gmail.com>
2023-02-20 16:04:30 +0200

    Uploding Main Files

---
+++ README.md
@@ -0,0 +1,12 @@
+# Template GLobal
+
+Template is training on HTML and CSS and some of Bootstrap And Owl Carousel And JQuray
+
+## This Template From [Free CSS](https://www.free-css.com/free-css-templates)
+
+- First Project Use Js After Starting Learn It
+- I Used Owl Carousel
+- All section Just Has opascity:0; And z-index:-10;
+- [Live Review](https://raw.githack.com/sonsalem/GLobale/main/GLobal-Free.html)
+
+**Have fun building!** 🚀
---
+++ js/JS-Free-Websites.js
@@ -0,0 +1,121 @@
+$('.bar').click(function(){
+    $('.aside').toggleClass('active')
+});
+$('.link1').click(function(){
+    // Links
+    $(this).addClass('active')
+    $(".link2").removeClass('active')
+    $(".link3").removeClass('active')
+    $(".link4").removeClass('active')
+    $(".link5").removeClass('active')
+    $(".button.btn-nav").removeClass('active')
+
+    // Pages
+    $(".home.section").addClass('active')
+    $(".work.section").removeClass('active')
+    $(".about.section").removeClass('active')
+    $(".contact.section").removeClass('active')
+    $(".hire.section").removeClass('active')
+
+    //body
+    $(".body").removeClass("active")
+});
+$('.link2').click(function(){
+    // Links
+    $(this).addClass('active')
+    $(".link1").removeClass('active')
+    $(".link3").removeClass('active')
+    $(".link4").removeClass('active')
+    $(".link5").removeClass('active')
+    $(".button.btn-nav").addClass('active')
+
+
+    //Pages
+    $(".work.section").addClass('active')
+    $(".home.section").removeClass('active')
+    $(".about.section").removeClass('active')
+    $(".contact.section").removeClass('active')
+    $(".hire.section").removeClass('active')
+
+    //body
+    $(".body").removeClass("active")
+});
+$('.link3').click(function(){
+    $(this).addClass('active')
+    $(".link1").removeClass('active')
+    $(".link2").removeClass('active')
+    $(".link4").removeClass('active')
+    $(".link5").removeClass('active')
+    $(".button.btn-nav").addClass('active')
+
+    // Pages
+    $(".about.section").addClass('active')
+    $(".work.section").removeClass('active')
+    $(".home.section").removeClass('active')
+    $(".contact.section").removeClass('active')
+    $(".hire.section").removeClass('active')
+
+    //body
+    $(".body").removeClass("active")
+});
+$('.link4').click(function(){
+    // Links
+    $(this).addClass('active')
+    $(".link1").removeClass('active')
+    $(".link2").removeClass('active')
+    $(".link3").removeClass('active')
+    $(".link5").removeClass('active')
+    $(".button.btn-nav").addClass('active')
+
+    // Pages
+    $(".contact.section").addClass('active')
+    $(".work.section").removeClass('active')
+    $(".about.section").removeClass('active')
+    $(".home.section").removeClass('active')
+    $(".hire.section").removeClass('active')
+
+    //body
+    $(".body").addClass("active")
+});
+$('.link5').click(function(){
+    // Links
+    $(this).addClass('active')
+    $(".link1").removeClass('active')
+    $(".link2").removeClass('active')
+    $(".link3").removeClass('active')
+    $(".link4").removeClass('active')
+    $(".button.btn-nav").removeClass('active')
+
+    // Pages
+    $(".hire.section").addClass('active')
+    $(".work.section").removeClass('active')
+    $(".about.section").removeClass('active')
+    $(".contact.section").removeClass('active')
+    $(".home.section").removeClass('active')
+
+    //body
+    $(".body").removeClass("active")
+});
+
+$(document).ready(function(){
+    $(".owl-work").owlCarousel({
+    loop:true,
+    nav:true,
+    dots:false,
+    autoplayTimeout:5000,
+    autoplay : true ,
+    center: true,
+    animateIn: 'fadeIn',
+    animateOut: 'fadeOut',
+    margin: 0,
+    responsive:{
+        0:{
+            items:1,
+        },
+        1199:{
+            items:3,
+        },
+        }
+    },
+    );
+});
